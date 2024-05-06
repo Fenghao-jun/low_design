000480d65b2d33e191acd026e20db28ce0b73e8b
enolaved <enolaved@gmail.com>
2024-05-06 18:51:54 +0800

    feat: 支持rules, 暂定自定义

--- src/design/components/Form/index.vue
+++ src/design/components/Form/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="form-container">
-    <el-form ref="formRef" v-bind="props.formProps">
+    <el-form
+      ref="formRef"
+      :model="formData"
+      :rules="rules"
+      v-bind="props.formProps"
+    >
       <template v-if="props.children?.length">
         <el-form-item
           v-for="item in props.children"
@@ -20,13 +25,37 @@
 </template>
 
 <script setup lang="ts" name="Form">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import type { FormInstance } from 'element-plus'
 import componentRegister from '@core/utils/component-regiter'
 import { FormProps } from './type'
 
 const props = defineProps<FormProps>()
-const emits = defineEmits(['updateModel'])
+const formData = ref({ ...(props.formProps?.model || {}) })
+
+const rules = computed(() => {
+  const children = props.children || []
+  const _rules = {}
+  children.forEach((child) => {
+    console.log('child: ', child)
+    const childProps = child.props
+    // eslint-disable-next-line array-callback-return
+    const childRules = (childProps?.rules || []).map((rule) => {
+      if (rule.validator) {
+        rule.validator =
+          rule.validator instanceof String
+            ? // eslint-disable-next-line no-new-func
+              new Function(rule.validator)
+            : rule.validator
+      } else {
+        return rule
+      }
+    })
+    _rules[childProps.fieldKey] = childRules
+  })
+  console.log(_rules)
+  return _rules
+})
 
 const formRef = ref<FormInstance>()
 
@@ -35,12 +64,19 @@ defineExpose({
   validateField: formRef.value?.validateField,
   resetFields: formRef.value?.resetFields,
   scrollToField: formRef.value?.scrollToField,
-  clearValidate: formRef.value?.clearValidate
+  clearValidate: formRef.value?.clearValidate,
+  initData(data?: Record<string, any>) {
+    if (data) {
+      formData.value = data
+    }
+  }
 })
 
 // 更新表单数据
-const updateModel = (fieldKey, newValue) =>
-  emits('updateModel', { fieldKey, newValue })
+const updateModel = (fieldKey, newValue) => {
+  formData.value[fieldKey] = newValue
+  console.log('updateModel: ', formData.value)
+}
 </script>
 
 <style lang="scss" scoped>

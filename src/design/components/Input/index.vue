2e9f461c1728b12e38da16e3e73887d26a4843d9
enolaved <enolaved@gmail.com>
2024-05-07 18:13:08 +0800

    feat: 新增一个input组件

---
+++ src/design/components/Input/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-input
+    v-model="value"
+    v-bind="props.inputProps"
+    @change="handleChange"
+  ></el-input>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { InputProps } from './type'
+const props = defineProps<InputProps>()
+// 上层组件监听 updateModel 方法来辅助更新表单的值
+const emit = defineEmits(['updateModel'])
+
+const value = ref(props.value || '')
+
+const handleChange = () => {
+  emit('updateModel', props.fieldKey, value.value)
+}
+</script>
---
+++ src/design/components/Input/type.ts
@@ -0,0 +1,5 @@
+export interface InputProps {
+  inputProps: Record<string, any>
+  value: string // 显示的值
+  fieldKey: string // 值的字段名
+}

b38b85c706da3183f127afa5ce8dd5d3a296264e
fenghaojun <1870292016@qq.com>
2024-05-09 15:08:29 +0800

    ✨ feat(Button组件): 处理props、click事件

--- src/design/components/Button/Button.vue
+++ src/design/components/Button/Button.vue
@@ -1,16 +1,22 @@
 <template>
-  <div @click="foo">321</div>
+  <ElButton v-bind="props" @click="handleClick">
+    {{ content }}
+  </ElButton>
 </template>
 
 <script setup lang="ts">
 import { defineEmits } from 'vue'
+import { ButtonProps } from './props'
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  content: 'hello',
+  type: 'primary'
+})
 
 const emit = defineEmits(['click'])
 
-const foo = () => {
-  console.log('emit')
-
-  emit('click', 'hello')
+const handleClick = () => {
+  emit('click')
 }
 </script>
 
---
+++ src/design/components/Button/props.ts
@@ -0,0 +1,5 @@
+import { ButtonProps as ElButtonProps } from 'element-plus'
+
+export interface ButtonProps extends ElButtonProps {
+  content?: string
+}

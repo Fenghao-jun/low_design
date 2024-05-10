fb227acbb9e07da98ae48b555a87c0778ef53c30
fenghaojun <1870292016@qq.com>
2024-05-10 18:28:59 +0800

    ✨ feat(事件): request事件加入路由数据

--- src/design-core/actions/RequestAction.ts
+++ src/design-core/actions/RequestAction.ts
@@ -12,6 +12,7 @@ import { get } from 'lodash-es'
 import { evaluate } from 'amis-formula'
 import { useApi } from '@design/hooks/useApi'
 import { checkArgs } from './common'
+import { useRoute } from 'vue-router'
 
 interface RequestParamsItem {
   key: string
@@ -30,21 +31,32 @@ interface IRequestAction extends ListenerAction {
   args: RequestActionArgs
 }
 
+function getPageData() {
+  const route = useRoute()
+  console.log('route: ', route)
+  return route
+}
+
 export class RequestAction implements RendererAction {
   async run(node: EventNode<IRequestAction>, eventData, initEventData) {
     try {
       checkArgs(node, 'request')
 
       const { params = [] } = node.actionConfig!.args
+      const mergeData = {
+        eventData,
+        initEventData,
+        pageData: getPageData().params
+      }
       const requestParams = params.reduce(
         (prev, cur) => {
           if (cur.formula) {
             // 执行公式
-            prev[cur.key] = evaluate(cur.formula, { eventData, initEventData })
+            prev[cur.key] = evaluate(cur.formula, mergeData)
 
             return prev
           }
-          prev[cur.key] = get({ eventData, initEventData }, cur.value)
+          prev[cur.key] = get(mergeData, cur.value)
           return prev
         },
         { ...eventData }
@@ -57,8 +69,11 @@ export class RequestAction implements RendererAction {
         data: requestParams
       })
 
+      console.log('res: ', res)
+
       excelEventFlow(findStatusNode(node.children), res.data, initEventData)
     } catch (error) {
+      console.log('error: ', error)
       excelEventFlow(
         findStatusNode(findStatusNode(node.children, 'error'), 'error'),
         null,

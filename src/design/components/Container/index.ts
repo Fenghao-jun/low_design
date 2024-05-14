3607ef5e33d2d5bea572ec4196b3f72c62cbdd1b
enolaved <enolaved@gmail.com>
2024-05-14 18:29:45 +0800

    feat: Container组件添加一些配置

--- src/design/components/Container/index.ts
+++ src/design/components/Container/index.ts
@@ -2,6 +2,13 @@ import Container from './index.vue'
 
 export { Container }
 
+const componentConfig = {
+  name: '容器',
+  key: 'container',
+  desc: '容器组件'
+}
+
 export default {
-  component: Container
+  component: Container,
+  componentConfig
 }

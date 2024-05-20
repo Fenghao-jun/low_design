6cf6e1d11db25e86ef46b06a351e3668fb47cac7
enolaved <enolaved@gmail.com>
2024-05-20 12:55:59 +0800

    test

--- remote-url.js
+++ remote-url.js
@@ -1,4 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 exports.demoRemoteUrl = isProd ?'demo@https://cdn.dataso.cn/static/remote-components/demo-vue-cli/demoComponentEntry.js' : 'demo@http://localhost:3001/demoComponentEntry.js'
-exports.shopRemoteUrl = isProd ?'shop@https://cdn.dataso.cn/static/remote-components/shop/entry.js' : 'shop@http://localhost:3001/entry.js'
+// exports.shopRemoteUrl = isProd ?'shop@https://cdn.dataso.cn/static/remote-components/shop/entry.js' : 'shop@http://localhost:3001/entry.js'
+exports.shopRemoteUrl = isProd ?'shop@https://test-topaz-theta-93.vercel.app/entry.js' : 'shop@http://localhost:3001/entry.js'
+

fea69909c17bc5351f1d65b7d2b91920b855d918
enolaved <enolaved@gmail.com>
2024-05-23 10:58:11 +0800

    feat: CRUD组件支持控制操作按钮的显隐

--- src/design/components/CRUD/CRUD.vue
+++ src/design/components/CRUD/CRUD.vue
@@ -10,6 +10,7 @@
       <template #operation="scope">
         <template v-for="(item, index) in operations" :key="index">
           <ElButton
+            v-if="handleOperationItemHidden(item, scope.row)"
             text
             type="primary"
             @click="handleActionClick(item, scope.row)"
@@ -56,6 +57,8 @@ const props = withDefaults(defineProps<CRUDProps>(), {
 
 const sortField = ref({ field: '', order: 'asc' })
 
+const store = usePageDataStore()
+
 const handleTableSortChange = (data: {
   column: any
   prop: string
@@ -88,7 +91,7 @@ const apiRequest = async (params) => {
   // 处理Scheme额外参数
   if (props.api.params) {
     const mergeData = {
-      pageData: usePageDataStore().getData()
+      pageData: store.getData()
     }
     otherParams = props.api.params.reduce((prev, cur) => {
       if (cur.formula) {
@@ -182,6 +185,23 @@ const getSelectedRow = () => {
   return selectRow
 }
 
+const handleOperationItemHidden = (
+  item: RowOperation,
+  row: Record<string, any>
+) => {
+  if (!item.hidden) {
+    return true
+  }
+
+  if (item.hidden?.type === 'formula') {
+    return item.hidden.formula ? evaluate(item.hidden.formula, row) : true
+  } else if (item.hidden.type === 'variable') {
+    return store.pageData[item.hidden.value]
+  } else {
+    return !!item.hidden.value
+  }
+}
+
 const clearSelectedRow = () => {
   return tableRef.value?.clearSelection()
 }
@@ -190,20 +210,13 @@ const search = () => {
   tableRef.value?.search()
 }
 
-const getSearchData = () => {
-  const params = tableRef.value?.searchParam || {}
-  return params
-}
-
 defineExpose({
   // 获取当前选中的行
   getSelectedRow,
   // 清空选中
   clearSelectedRow,
   // 搜索
-  search,
-  // 获取搜索参数
-  getSearchData
+  search
 })
 </script>
 
